a211eb9275a56d1270ee6f3be63f11966615f696
Nahuel Chawi Rojas <nadaro@outlook.com>
2023-10-11 14:47:53 -0300

    animator

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private bool isMoving;
+    public float speed;
+    private Vector2 input;
+    private void Update() {
+        if(!isMoving)
+        {
+            input.x = Input.GetAxisRaw("Horizontal");
+            input.y = Input.GetAxisRaw("Vertical");
+            if(input.x != 0)
+            {
+                input.y = 0;
+            }
+            if(input != Vector2.zero)
+            {
+                var targetPosition = transform.position;
+                targetPosition.x += input.x;
+                targetPosition.y += input.y;
 
-    // Update is called once per frame
-    void Update()
+                StartCoroutine(MoveTowards(targetPosition));
+            }
+        }
+    }
+    IEnumerator MoveTowards(Vector3 destination)
     {
-        
+        isMoving = true;
+        while(Vector3.Distance(transform.position, destination) > Mathf.Epsilon)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, destination,speed * Time.deltaTime);
+            yield return null;
+        }
+        transform.position = destination;
+        isMoving = false;
     }
 }
